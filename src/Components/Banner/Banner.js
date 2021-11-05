1a04288d5fb78b1c35ab0ac6f4de4f0c195eb14c
Dexterpolygon1605 <nelsonlopescp2020@gmail.com>
2021-11-05 19:23:26 +0000

    homepage optimazed 02

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -6,15 +6,15 @@ const Banner = () => {
     return (
         <div className="banner fixed w-50 h-auto br4 mt4-m">
             <article class="mw7 center ph3 ph3-ns tc br2 pv5 white mt0">
-                <img className="grow w-60 w-90-m" src={Profile} alt="Profile" />
-                <h1 class="fw6 f3 f1-ns f4-m lh-title mt3 mb3">
+                <img className="grow w-100 w-60-ns w-90-m" src={Profile} alt="Profile" />
+                <h1 class="fw6 f4 f1-ns f4-m lh-title mt3 mb3">
                     Hi! Im Nelson Lopes,
                 </h1>
-                <h2 class="fw2 f2 f5-m lh-copy mt0 mb3">
+                <h2 class="fw2 f4 f5-m lh-copy mt0 mb3">
                     Here's where I showcase my projects
                 </h2>
                 <div>
-                    <a class="shadow-5 f3 br-pill no-underline white ba b--white grow pv2 ph5 dib ml3 mr3 ml0-m grow"
+                    <a class="shadow-5 f5 f4-ns br-pill no-underline white ba b--white grow pv2 ph4 dib ml3 ml3-ns mr3 ml0-m grow"
                         href="0#">
                         View my Projects 
                     </a>
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,10 +4,10 @@ import './Navigation.css';
 const Navigation = () => {
     return (
         <div>
-            <nav className="pa3 pa4-ns">
-                <a className="navb link dim white b f4 f1-ns f4-m dib mr3 fl" href="0#" title="Home">NLProjects</a>
-                <a className="navb shadow-5 link dim white f6 f5-ns dib mr3 fr ml4 br-pill ba pv2 ph3 grow" href="0#" title="About">Git Hub</a>
-                <a className="navb shadow-5 link dim white f6 f5-ns dib fr br-pill ba pv2 ph3 grow" href="0#" title="Contact">LinkedIn</a>
+            <nav className="mt2 mt0-ns ml2 ml0-ns pa3 pa4-ns">
+                <a className="navb link dim white b f3 f1-ns f4-m dib mr3 fl mt1 mt0-ns" href="0#" title="Home">NLProjects</a>
+                <a className="navb shadow-5 link dim white f6 f5-ns dib mr1 mr3-ns fr ml2 ml4-ns br-pill ba pv2 ph2 ph3-ns grow" href="0#" title="About">Git Hub</a>
+                <a className="navb shadow-5 link dim white f6 f5-ns dib fr br-pill ba pv2 ph2 ph3-ns grow" href="0#" title="Contact">LinkedIn</a>
             </nav>
         </div>
     )
