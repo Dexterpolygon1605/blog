d448bf91ea3c9c40a6782e722bc5fbd1bc6f1477
Dexterpolygon1605 <nelsonlopescp2020@gmail.com>
2021-11-05 00:32:01 +0000

    Navigation bar

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Navigation = () => {
+    return (
+        <nav class="pa3 pa4-ns bg-white">
+            <a class="link dim black b f6 f5-ns dib mr3" href="0#" title="Home">NLProjects</a>
+            <a class="link dim gray    f6 f5-ns dib mr3" href="0#" title="Works">Works</a>
+            <a class="link dim gray    f6 f5-ns dib mr3" href="0#" title="About">About</a>
+            <a class="link dim gray    f6 f5-ns dib" href="0#" title="Contact">Contact</a>
+        </nav>
+    )
+}
+
+export default Navigation;
