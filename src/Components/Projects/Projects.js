8257a79ac30585ca430e96593e435b57f39b4ff4
Dexterpolygon1605 <nelsonlopescp2020@gmail.com>
2021-11-08 17:40:14 +0000

    project cards updated 01

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import homepage from './homepage.png'
 import rpage from './rpage.png'
+import prpage from './prpage.png'
 
 
 const contentState = {
@@ -42,11 +43,11 @@ class Projects extends Component {
                         ?
                         <a href="https://smartbrainnl.herokuapp.com/" target="blank"> <img src={homepage} class="w-100 db br3 shadow-2 grow" alt="Smartbrain" /> </a>
                         :
-                        <p className="desc tl w-70 center" onClick={() => this.onContentChange('picture')} > 
-                        This website can detect faces in pictures with a API, it also has the sign in and register functions, which works with a server and database, so if you register
-                        you'll be able to sign in right away. The database will store how many times you detected a face in a picture, so if you entry an image that doesn't have a face, it
-                        doesn't store that try. 
-                         Made with: <b className="light-blue">HTML5, CSS3, JavaScript(React), Node.js, PostgresSQL, Git bash and Heroku. </b> </p>
+                        <p className="desc tl w-70 center" onClick={() => this.onContentChange('picture')} >
+                            This website can detect faces in pictures with a API, it also has the sign in and register functions, which works with a server and database, so if you register
+                            you'll be able to sign in right away. The database will store how many times you detected a face in a picture, so if you entry an image that doesn't have a face, it
+                            doesn't store that try.
+                            Made with: <b className="light-blue">HTML5, CSS3, JavaScript(React), Node.js, PostgresSQL, Git bash and Heroku. </b> </p>
                     }
 
                     <div class="pa3">
@@ -69,14 +70,41 @@ class Projects extends Component {
                         ?
                         <a href="https://dexterpolygon1605.github.io/robotfriends/" target="blank"> <img src={rpage} class="w-100 db br3 shadow-2 grow" alt="RobotFriends" /> </a>
                         :
-                        <p className="desc tl w-70 center" onClick={() => this.onContentChange('picture')} > 
-                         This is a simple website that has a functional search bar and a grid with JSON objects.
-                         Made with: <b className="light-blue">HTML5, CSS3, JavaScript(React), Git bash and JSON</b> </p>
+                        <p className="desc tl w-70 center" onClick={() => this.onContentChange('picture')} >
+                            This is a simple website that has a functional search bar and a grid with JSON objects.
+                            Made with: <b className="light-blue">HTML5, CSS3, JavaScript(React), Git bash and JSON</b> </p>
                     }
 
                     <div class="pa3">
                         <a href="https://dexterpolygon1605.github.io/robotfriends/" class="pointer light-blue link dim lh-title" target="blank">Published</a>
                         <p className="white"> React Project </p>
+                    </div>
+                </article>
+                <article class="bg-gray tc dib br3 pa0 ma2 bw2 shadow-5 mw5 mw6-ns mw5-m">
+                    <div class="pv2 ph3">
+                        <h1 class="f3 f2-ns f3-m tracked light-blue">PortoRequintado</h1>
+                        {change === 'picture'
+                            ?
+                            <h1 onClick={() => this.onContentChange('details')} class="tl f6 white grow pointer">More details...</h1>
+                            :
+                            <h1 onClick={() => this.onContentChange('picture')} class="tl f6 white grow pointer">Open image...</h1>
+                        }
+                    </div>
+
+                    {change === 'picture'
+                        ?
+                        <a href="https://portorequintado.netlify.app/" target="blank"> <img src={prpage} class="w-100 db br3 shadow-2 grow" alt="PortoRequintado" /> </a>
+                        :
+                        <p className="desc tl w-70 center" onClick={() => this.onContentChange('picture')} >
+                            This is a website that I made in group for internship and I was the one who writed all the code. It was proposed to have a public target
+                            and we choose the more exquisite people that visit Porto.
+                            The project is not finished, it was our first complex project.
+                            Made with: <b className="light-blue">HTML5, CSS3, JavaScript</b> </p>
+                    }
+
+                    <div class="pa3">
+                        <a href="https://portorequintado.netlify.app/" class="pointer light-blue link dim lh-title" target="blank">Published</a>
+                        <p className="white"> Website Project </p>
                     </div>
                 </article>
             </div>
