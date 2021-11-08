63fcc74075f014184e8b39c18b2d9d3b68d130e8
Dexterpolygon1605 <nelsonlopescp2020@gmail.com>
2021-11-08 18:44:06 +0000

    project cards updated 05

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer class="z-2 fixed bg-near-black white-80 pv0 pv0-l ph4 w-100 mt6">
             <p class="f6">
                 <span class="tc">nelsonlopescp@gmail.com
-                <p class="link white-80 tc">910480778</p>
+                <p class="link white-80 tc mt1">910480778</p>
                 </span>
             </p>
         </footer>
