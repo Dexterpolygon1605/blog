74507af81d05408119639bdb0ddeff6d29612a5d
Dexterpolygon1605 <nelsonlopescp2020@gmail.com>
2021-11-10 14:04:27 +0000

    optimized footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <footer class="num z-2 fixed white-80 pv0 pv0-l ph4 w-100 mt6">
             <p class="f6">
-                <span class="tc">nelsonlopescp@gmail.com
-                <p class="link tc mt1">910480778</p>
+                <span href="mailto:nelsonlopescp2020@gmail.com" class="tc">nelsonlopescp@gmail.com
+                <p href="tel:+351910480778" class="link tc mt1">910480778</p>
                 </span>
             </p>
         </footer>
