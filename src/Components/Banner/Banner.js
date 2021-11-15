43ecb408c25b4caec66ac850b4206b2bcd23ce9c
Dexterpolygon1605 <nelsonlopescp2020@gmail.com>
2021-11-15 18:59:00 +0000

    link optimized 02

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -18,7 +18,7 @@ class Banner extends React.Component {
                         Here's where I showcase my projects
                     </h2>
                     <div>
-                        <a href="https://drive.google.com/file/d/1xVQ8eRQK5Zc4_FqXTxW2PfzSWKUum1p9/view?usp=sharing" class="btnbanner pointer shadow-5 f5 f4-ns f5-m br-pill no-underline white ba b--white grow pv2 ph4 dib ml3 ml3-ns mr2 mr3-l ml0-m grow mb6" target="_blank">
+                        <a href="https://drive.google.com/file/d/1xVQ8eRQK5Zc4_FqXTxW2PfzSWKUum1p9/view?usp=sharing" class="btnbanner pointer shadow-5 f5 f4-ns f5-m br-pill no-underline white ba b--white grow pv2 ph4 dib ml3 ml3-ns mr2 mr3-l ml0-m grow mb6" target="_blank" rel="noreferrer">
                             Resume
                         </a>
                     </div>
